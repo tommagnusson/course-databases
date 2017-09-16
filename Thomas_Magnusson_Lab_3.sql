293bace00326d4663354671de18e72a033a2c8cf
Thomas Magnusson <thomas.magnusson1@marist.edu>
2017-09-16 17:07:56 -0400

    lab 3 complete 9.16.17

---
+++ Thomas_Magnusson_Lab_3.sql
@@ -0,0 +1,34 @@
+﻿select ordno, totalusd 
+from orders;
+
+select name, city
+from agents
+where name='Smith';
+
+select pid, name, priceusd
+from products
+where qty>200010;
+
+select name, city
+from customers
+where city='Duluth';
+
+select name
+from agents
+where city!='New York' and city!='Duluth';
+
+select *
+from products
+where city!='Duluth' and city!='Dallas' and priceusd>=1;
+
+select *
+from orders
+where month='Mar' or month='May';
+
+select *
+from orders
+where month='Feb' and totalusd>=500;
+
+select *
+from orders
+where cid='c005';
